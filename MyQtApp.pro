4d30c01a1e8ab9a91eccaee914498ca2ad4125a0
Mauro Marssola <marssolinha@hotmail.com>
2019-10-21 10:50:56 -0300

    QModules added

--- MyQtApp.pro
+++ MyQtApp.pro
@@ -13,6 +13,11 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+include(../QModules/src/components/components.pri)
+include(../QModules/src/js/js.pri)
+include(../QModules/themes/UiKit.pri)
+include(../QModules/icons/material_icons.pri)
+
 SOURCES += \
         main.cpp
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
+    QGuiApplication::setApplicationName("Who am I");
+    QGuiApplication::setOrganizationName("com.marssola");
+    QGuiApplication::setOrganizationName("Marssola");
+    QGuiApplication::setApplicationVersion("1.0");
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
     QGuiApplication app(argc, argv);
 
@@ -14,6 +21,13 @@ int main(int argc, char *argv[])
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
+
+    QIcon::setThemeSearchPaths({ ":/" });
+    QIcon::setThemeName("material-round");
+    QQuickStyle::setStyle(":/UiKit");
+    QQuickStyle::setFallbackStyle("Default");
+    engine.addImportPath(":/");
+
     engine.load(url);
 
     return app.exec();
